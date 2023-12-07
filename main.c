3d5d420634c1316aadf87b158aea3171ecd2cced
Spatulox <M78stormtrooper@laposte.net>
2023-12-08 00:03:40 +0100

    now use struct for buttons

--- main.c
+++ main.c
@@ -5,15 +5,50 @@
 #include "src/includes/global_functions.h"
 #include "src/includes/interface.h"
 
-int main(int argc, char** argv) {
 
-    SDL_Event event;
+
+// Structure pour représenter un bouton
+typedef struct {
+    SDL_Rect rect;
+    char* text;
+    int nb;
+    // Autres propriétés du bouton
+} Button;
+
+// Tableau de boutons
+Button buttons[3];
+
+
+void createMenu(SDL_Window * window, SDL_Renderer* renderer, int width, int height, SDL_DisplayMode dm, char* fontPath) {
+    buttons[0].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 3) - (height / 2), width, height - (dm.h / 30)};
+    buttons[0].text = "Jouer";
+
+    buttons[1].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 2) - (height / 2), width, height - (dm.h / 30)};
+    buttons[1].text = "Parametres";
+
+    buttons[2].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 1.5) - (height / 2), width, height - (dm.h / 30)};
+    buttons[2].text = "Quitter le jeu";
+
+    SDL_Color fColor = {0, 0, 0};
+    // Dessiner les boutons sur le renderer
+    for (int i = 0; i < 3; i++) {
+        createTextRectButton(&buttons[i].rect, window, renderer, dm, 255, 255, 255, 255, buttons[i].text, fontPath, fColor);
+    }
+}
+
+
+
+
+
+
+int main(int argc, char** argv) {
 
     Log("---------------------------");
     Log("Starting Secret Agent");
 
+    SDL_Event event;
 
-
+    // ------------Initialize SDL and SDL addons------------
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
         Log("ERROR : Impossible to initialize SDL");
@@ -28,8 +63,7 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-
-    // Récupère la taille de l'écran
+    // ------------Retrieve screen size------------
     SDL_DisplayMode dm;
     if (SDL_GetDesktopDisplayMode(0, &dm) != 0) {
         Log("WARNING : Impossible to retrieve the screen size, set standard size");
@@ -37,7 +71,7 @@ int main(int argc, char** argv) {
         dm.h = 600; // Default height
     }
 
-    // Crée une fenêtre prenant la taille de l'écran
+    // Create a window with the screen size
     SDL_Window* window = SDL_CreateWindow("Secret Agent", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, dm.w, dm.h, SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         Log("ERROR : Impossible to create the Secret-Agent window");
@@ -52,54 +86,87 @@ int main(int argc, char** argv) {
     }
 
 
-
+    // ------------Initialize the menu------------
     int width = 400;
     int height = 150;
 
-    // Need to create a function which create buttons
-    //createTextRectButton(renderer, float x, float y, float width, float height, font, textColor);
-    //SDL_Rect rect = { (dm.w/2)-(width/2), (dm.h/2)-(height/2), width, height };
-    //createTextRectButton(SDL_Rect*, SDL_Window*, SDL_Renderer *, SDL_DisplayMode, int rectColorRed, int rectColorGreen, int rectColorBlue, int rectTransparency, TTF_Font*, SDL_Color);
-
-
-    // Create the rectangle
-    SDL_Rect rect = { (dm.w/2)-(width/2), (dm.h/4)-(height/2), width, height }; // Remplacez x, y, width et height par les coordonnées et les dimensions de votre rectangle
-    SDL_Color fColor = {0, 255, 0};
-    createTextRectButton(&rect, window, renderer, dm, 0, 0, 255, 255, "Hello SDL PUTAIN", "../src/fonts/arial.ttf", fColor);
+    createMenu(window, renderer, width, height, dm, "../src/fonts/arial.ttf");
 
 
     int isRunning = 1;
     int menuGameEtc = 0; // 0 => Menu, 1 => Jouer, 2 => Paramètres
 
-    // Boucle du jeu
-//    SDL_RenderPresent(renderer);
-//    SDL_Delay(10000);
 
-//    destroySDL(window, renderer, texture);
-
-    while (isRunning) {
+    while(isRunning){
         SDL_RenderPresent(renderer);
+        // Listen to events
         if (SDL_PollEvent(&event)) {
 
+            // Do something when event is call
             if (event.type == SDL_QUIT) {
                 isRunning = 0;
             } else if (event.type == SDL_MOUSEBUTTONDOWN) {
                 int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY);
                 SDL_Point clickPoint = {mouseX, mouseY};
-                if (SDL_PointInRect(&clickPoint, &rect)) {
+
+                for (int i = 0; i < 3; i++) {
+                    if (SDL_PointInRect(&clickPoint, &(buttons[i].rect))) {
+                        // Le bouton i a été cliqué
+                        if (strcmp(buttons[i].text, "Jouer") == 0) {
+                            Log("Jouer cliqué !");
+                            isRunning = 0;
+                            break;
+                        } else if (strcmp(buttons[i].text, "Parametres") == 0) {
+                            Log("Parametres cliqué !");
+                            isRunning = 0;
+                            break;
+                        } else if (strcmp(buttons[i].text, "Quitter le jeu") == 0) {
+                            Log("Quitter cliqué !");
+                            isRunning = 0;
+                            break;
+                        }
+                    }
+                }
+                /*
+                // Retrieve data of the click
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                SDL_Point clickPoint = {mouseX, mouseY};
+
+                // Execute some kind of things
+                // If it's the play button
+                if (SDL_PointInRect(&clickPoint, &menuButton)) {
                     Log("Cliqué !");
                     isRunning = 0;
                 }
+                // If it's the parameters button
+                if (SDL_PointInRect(&clickPoint, &parameterButton)) {
+                    Log("Cliqué !");
+                    isRunning = 0;
+                }
+                // If it's the Quit button
+                if (SDL_PointInRect(&clickPoint, &quitButton)) {
+                    Log("Cliqué !");
+                    isRunning = 0;
+                }
+                 */
             }
         }
 
     }
 
+    // CLear le renderer
+    //SDL_RenderClear(renderer);
+    // Met a jour le renderer
+    //SDL_RenderPresent(renderer);
+
+
     // Wait 10  seconds
+    // Log("10 seconds of delay");
+    //SDL_RenderPresent(renderer);
     //SDL_Delay(10000);
 
-    //SDL_DestroyTexture(texture);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
