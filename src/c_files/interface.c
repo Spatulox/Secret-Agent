5831bd6874f98b224aaff8ad6990ccee2173ff16
Spatulox <M78stormtrooper@laposte.net>
2023-12-08 00:03:27 +0100

    moved createTextButton into interface.h

--- src/c_files/interface.c
+++ src/c_files/interface.c
@@ -3,9 +3,123 @@
 //
 
 
+#include <SDL.h>
+#include <SDL_ttf.h>
+
 #include "../includes/interface.h"
-#include "SDL.h"
+#include "../includes/global_functions.h"
 
-void createRectButton(SDL_Renderer* renderer, int x, int y, int width, int height, int r, int g, int b, int a){
+// ----------------------------------------------------------- //
+void createTextRectButton(SDL_Rect* rect, SDL_Window* window, SDL_Renderer * renderer, SDL_DisplayMode dm, int rectColorRed, int rectColorGreen, int rectColorBlue, int rectTransparency, char* string,  char* fontPath, SDL_Color fColor){
 
+    //Log("createTextRectButton");
+
+    if(rectColorRed > 255){
+        rectColorRed = 255;
+        Log("Too high value for rectColorRed");
+    }
+    if (rectColorRed < 0){
+        rectColorRed = 0;
+        Log("Too low value for rectColorRed");
+    }
+    if(rectColorGreen > 255){
+        rectColorGreen = 255;
+        Log("Too high value for rectColorGreen");
+    }
+    if (rectColorGreen < 0){
+        rectColorGreen = 0;
+        Log("Too low value for rectColorGreen");
+    }
+    if(rectColorBlue > 255){
+        rectColorBlue = 255;
+        Log("Too high value for rectColorBlue");
+    }
+    if (rectColorBlue < 0){
+        rectColorBlue = 0;
+        Log("Too low value for rectColorBlue");
+    }
+    if(rectTransparency > 255){
+        rectTransparency = 255;
+        Log("Too high value for rectTransparency");
+    }
+    if (rectTransparency < 0){
+        rectTransparency = 0;
+        Log("Too low value for rectTransparency");
+    }
+
+
+    SDL_SetRenderDrawColor(renderer, rectColorRed, rectColorGreen, rectColorBlue, rectTransparency); // Remplacez r, g, b et a par les valeurs de couleur souhaitées
+    SDL_RenderDrawRect(renderer, rect);
+    SDL_RenderFillRect(renderer, rect);
+
+    // Load font
+    TTF_Font* font = TTF_OpenFont("../src/fonts/arial.ttf", 25);
+    // TTF_Font* font = TTF_OpenFont(fontPath, 25);
+    if (font == NULL){
+        Log("ERROR : Impossible to import the font");
+        TTF_CloseFont(font);
+        destroySDL(window, renderer, NULL);
+    }
+
+
+
+    SDL_Surface* surface = TTF_RenderText_Solid(font, string, fColor);
+    if (surface == NULL){
+        Log("ERROR : Impossible to create the surface for the texture");
+        TTF_CloseFont(font);
+        SDL_FreeSurface(surface);
+        destroySDL(window, renderer, NULL);
+    }
+    TTF_CloseFont(font);
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+    if (texture == NULL){
+        Log("ERROR : Impossible to create the texture");
+        TTF_CloseFont(font);
+        SDL_FreeSurface(surface);
+        destroySDL(window, renderer, texture);
+    }
+
+    SDL_FreeSurface(surface);
+
+
+
+    if (SDL_QueryTexture(texture, NULL, NULL, &dm.w, &dm.h) != 0){
+        Log("ERROR : Impossible load the texte in RAM");
+        destroySDL(window, renderer, texture);
+    }
+
+
+
+    if (SDL_RenderCopy(renderer, texture, NULL, rect) != 0){
+        Log("ERROR : Impossible to print the texte");
+        destroySDL(window, renderer, texture);
+    }
+
+    //Log("createTextRectButton finished !");
 }
+
+// ----------------------------------------------------------- //
+
+/*
+void createMenu(SDL_Window * window, SDL_Renderer* renderer, SDL_DisplayMode dm){
+    int width = 400;
+    int height = 150;
+
+    SDL_Color fColor = {0, 0, 0};
+    // Create the rectangle
+    SDL_Rect menuButton = { (dm.w/2)-(width/2), (dm.h/3)-(height/2), width, height-(dm.h/30) }; // Remplacez x, y, width et height par les coordonnées et les dimensions de votre rectangle
+    createTextRectButton(&menuButton, window, renderer, dm, 255, 255, 255, 255, "Jouer", "../src/fonts/arial.ttf", fColor);
+
+    SDL_Rect parameterButton = { (dm.w/2)-(width/2), (dm.h/2)-(height/2), width, height-(dm.h/30) }; // Remplacez x, y, width et height par les coordonnées et les dimensions de votre rectangle
+    createTextRectButton(&parameterButton, window, renderer, dm, 255, 255, 255, 255, "Parametres", "../src/fonts/arial.ttf", fColor);
+
+    SDL_Rect quitButton = { (dm.w/2)-(width/2), (dm.h/1.5)-(height/2), width, height-(dm.h/30) }; // Remplacez x, y, width et height par les coordonnées et les dimensions de votre rectangle
+    createTextRectButton(&quitButton, window, renderer, dm, 255, 255, 255, 255, "Quitter le jeu", "../src/fonts/arial.ttf", fColor);
+}
+ */
+
+// ----------------------------------------------------------- //
+
+
+// Fonction pour créer les boutons
--- src/includes/interface.h
+++ src/includes/interface.h
@@ -5,8 +5,6 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include "SDL.h"
-
-void createRectButton(SDL_Renderer* renderer, int x, int y, int width, int height, int red, int green, int blue, int transparency);
-
+void createTextRectButton(SDL_Rect*, SDL_Window*, SDL_Renderer *, SDL_DisplayMode, int rectColorRed, int rectColorGreen, int rectColorBlue, int rectTransparency, char*, char*, SDL_Color);
+//void createMenu(SDL_Window *, SDL_Renderer*, SDL_DisplayMode);
 #endif //INTERFACE_H
