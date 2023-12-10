b4ed52ba2c729a726c1b5650eab499490642b58a
Spatulox <M78stormtrooper@laposte.net>
2023-12-10 14:22:41 +0100

    new createMenu Function

--- main.c
+++ main.c
@@ -2,41 +2,13 @@
 #include <SDL.h>
 #include <SDL_ttf.h>
 // #include <curl/curl.h>
+
+// Functions
 #include "src/includes/global_functions.h"
 #include "src/includes/interface.h"
 
-
-
-// Structure pour représenter un bouton
-typedef struct {
-    SDL_Rect rect;
-    char* text;
-    int nb;
-    // Autres propriétés du bouton
-} Button;
-
-// Tableau de boutons
-Button buttons[3];
-
-
-void createMenu(SDL_Window * window, SDL_Renderer* renderer, int width, int height, SDL_DisplayMode dm, char* fontPath) {
-    buttons[0].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 3) - (height / 2), width, height - (dm.h / 30)};
-    buttons[0].text = "Jouer";
-
-    buttons[1].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 2) - (height / 2), width, height - (dm.h / 30)};
-    buttons[1].text = "Parametres";
-
-    buttons[2].rect = (SDL_Rect){(dm.w / 2) - (width / 2), (dm.h / 1.5) - (height / 2), width, height - (dm.h / 30)};
-    buttons[2].text = "Quitter le jeu";
-
-    SDL_Color fColor = {0, 0, 0};
-    // Dessiner les boutons sur le renderer
-    for (int i = 0; i < 3; i++) {
-        createTextRectButton(&buttons[i].rect, window, renderer, dm, 255, 255, 255, 255, buttons[i].text, fontPath, fColor);
-    }
-}
-
-
+// Structures
+#include "src/includes/buttons.h"
 
 
 
@@ -90,47 +62,55 @@ int main(int argc, char** argv) {
     int width = 400;
     int height = 150;
 
-    createMenu(window, renderer, width, height, dm, "../src/fonts/arial.ttf");
+    // Tableau de boutons
+    Button buttons[3];
+    createMenu(window, renderer, width, height, dm, "../src/fonts/arial.ttf", buttons);
 
 
     int isRunning = 1;
-    int menuGameEtc = 0; // 0 => Menu, 1 => Jouer, 2 => Paramètres
+    int menuState = 0; // 0 => Menu, 1 => Jouer, 2 => Paramètres
 
 
     while(isRunning){
+        // Render the renderer
         SDL_RenderPresent(renderer);
+
         // Listen to events
         if (SDL_PollEvent(&event)) {
 
             // Do something when event is call
             if (event.type == SDL_QUIT) {
                 isRunning = 0;
-            } else if (event.type == SDL_MOUSEBUTTONDOWN) {
+            }
+            else if (event.type == SDL_MOUSEBUTTONDOWN)
+            {
                 int mouseX, mouseY;
                 SDL_GetMouseState(&mouseX, &mouseY);
                 SDL_Point clickPoint = {mouseX, mouseY};
 
-                for (int i = 0; i < 3; i++) {
-                    if (SDL_PointInRect(&clickPoint, &(buttons[i].rect))) {
-                        // Le bouton i a été cliqué
-                        if (strcmp(buttons[i].text, "Jouer") == 0) {
-                            Log("Jouer cliqué !");
-                            isRunning = 0;
-                            break;
-                        } else if (strcmp(buttons[i].text, "Parametres") == 0) {
-                            Log("Parametres cliqué !");
-                            isRunning = 0;
-                            break;
-                        } else if (strcmp(buttons[i].text, "Quitter le jeu") == 0) {
-                            Log("Quitter cliqué !");
-                            isRunning = 0;
-                            break;
+                if (menuState == 0) {
+                    for (int i = 0; i < 3; i++) {
+                        if (SDL_PointInRect(&clickPoint, &(buttons[i].rect))) {
+                            // Le bouton i a été cliqué
+                            if (strcmp(buttons[i].text, "Jouer") == 0) {
+                                Log("Jouer cliqué !");
+                                menuState = 1;
+                                break;
+                            } else if (strcmp(buttons[i].text, "Parametres") == 0) {
+                                Log("Parametres cliqué !");
+                                isRunning = 0;
+                                menuState = 2;
+                                break;
+                            } else if (strcmp(buttons[i].text, "Quitter le jeu") == 0) {
+                                Log("Quitter cliqué !");
+                                isRunning = 0;
+                                break;
+                            }
                         }
                     }
                 }
             }
         }
-
     }
 
     // CLear le renderer
--- src/includes/interface.h
+++ src/includes/interface.h
@@ -5,6 +5,8 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
+#include "../includes/buttons.h"
+
 void createTextRectButton(SDL_Rect*, SDL_Window*, SDL_Renderer *, SDL_DisplayMode, int rectColorRed, int rectColorGreen, int rectColorBlue, int rectTransparency, char*, char*, SDL_Color);
-//void createMenu(SDL_Window *, SDL_Renderer*, SDL_DisplayMode);
+void createMenu(SDL_Window *, SDL_Renderer*, int width, int height, SDL_DisplayMode, char*, Button buttons[]);
 #endif //INTERFACE_H
