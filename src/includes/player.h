43bec7e188bca0c50021af5be5dce9c90fbe848a
Spatulox <M78stormtrooper@laposte.net>
2024-01-13 17:48:14 +0100

    add player movement

--- src/includes/player.h
+++ src/includes/player.h
@@ -2,7 +2,13 @@
 // Created by M78st on 13/01/2024.
 //
 
-#ifndef MAIN_C_PLAYER_H
-#define MAIN_C_PLAYER_H
+#ifndef PLAYER_H
+#define PLAYER_H
 
-#endif //MAIN_C_PLAYER_H
+#include "../includes/structPlayer.h"
+
+int loadPlayer(SDL_Renderer* renderer,  SDL_DisplayMode dm, Player * player);
+void rightPlayer(SDL_Renderer* renderer,  SDL_DisplayMode dm, Player * player);
+void leftPlayer(SDL_Renderer* renderer,  SDL_DisplayMode dm, Player * player);
+
+#endif //PLAYER_H
---
+++ src/includes/structPlayer.h
@@ -0,0 +1,25 @@
+//
+// Created by M78st on 13/01/2024.
+//
+
+#ifndef STRUCTPLAYER_H
+#define STRUCTPLAYER_H
+
+typedef struct {
+    int x;
+    int y;
+} Point;
+
+typedef struct {
+    int height;
+    int width;
+} Size;
+
+typedef struct {
+    Point coordinates;
+    Size size;
+    char * pathToPngFile;
+    int verticalFlip;
+} Player;
+
+#endif //STRUCTPLAYER_H
