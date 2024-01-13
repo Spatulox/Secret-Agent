d90472c06b78f1dd16fbdaaafb408210e20b8414
Spatulox <M78stormtrooper@laposte.net>
2024-01-13 15:50:16 +0100

    add menu.c and menu.h to update the menuState and things in function

---
+++ src/c_files/menu.c
@@ -0,0 +1,65 @@
+//
+// Created by Marc on 13/01/2024.
+//
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+#include "../includes/buttons.h"
+#include "../includes/global_functions.h"
+
+void changeMenuState(int * isRunning, int * menuState, int * difficulty, SDL_Thread *audio, SDL_Point clickPoint, Button buttons[]){
+
+    if (*menuState == 0) {
+        for (int i = 0; i < 3; i++) {
+            if (SDL_PointInRect(&clickPoint, &(buttons[i].rect))) {
+                if (strcmp(buttons[i].text, "Jouer") == 0) {
+                    Log("Jouer cliqué !");
+                    *menuState = 1;
+                    SDL_DetachThread(audio);
+                    Mix_HaltMusic();
+                    break;
+                } else if (strcmp(buttons[i].text, "Parametres") == 0) {
+                    Log("Parametres cliqué !");
+                    *menuState = 2;
+                    Mix_HaltMusic();
+                    break;
+                } else if (strcmp(buttons[i].text, "Quitter le jeu") == 0) {
+                    Log("Quitter cliqué !");
+                    *isRunning = 0;
+                    Mix_HaltMusic();
+                    break;
+                }
+            }
+        }
+    } else if (*menuState == 1) {
+        for (int i = 3; i < 6; i++) {
+            if (SDL_PointInRect(&clickPoint, &(buttons[i].rect))) {
+                if (strcmp(buttons[i].text, "Facile") == 0) {
+                    Log("Facile cliqué !");
+                    *difficulty = 1;
+                    *menuState = 3;
+                    SDL_DetachThread(audio);
+                    Mix_HaltMusic();
+                    break;
+                } else if (strcmp(buttons[i].text, "Normal") == 0) {
+                    Log("Normal cliqué !");
+                    *difficulty = 2;
+                    *menuState = 3;
+                    Mix_HaltMusic();
+                    break;
+                } else if (strcmp(buttons[i].text, "Difficile") == 0) {
+                    Log("Difficile cliqué !");
+                    *difficulty = 3;
+                    *menuState = 3;
+                    Mix_HaltMusic();
+                    break;
+                } else {
+                    Log("Pas de bouton");
+                }
+            }
+            else{
+                Log("Meh");
+            }
+        }
+    }
+}
---
+++ src/includes/menu.h
@@ -0,0 +1,10 @@
+//
+// Created by M78st on 13/01/2024.
+//
+
+#ifndef MAIN_C_MENU_H
+#define MAIN_C_MENU_H
+
+void changeMenuState(int * isRunning, int * menuState, int *difficulty, SDL_Thread *audio, SDL_Point clickPoint, Button buttons[]);
+
+#endif //MAIN_C_MENU_H
