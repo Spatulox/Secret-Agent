c76a564737a0c0f09fa318ee50c807ca6f6765ef
Kosta <kosta.djordjevic2003@gmail.com>
2024-01-22 17:55:33 +0100

    update changeMenu prototype

--- src/includes/menu.h
+++ src/includes/menu.h
@@ -5,6 +5,8 @@
 #ifndef MAIN_C_MENU_H
 #define MAIN_C_MENU_H
 
-void changeMenuState(int * isRunning, int * menuState, int *difficulty, SDL_Thread *audio, SDL_Point clickPoint, Button buttons[]);
+#include "pressed_key.h"
+
+void changeMenuState(int * isRunning, int * menuState, int *difficulty, SDL_Thread *audio, SDL_Point clickPoint, Button buttons[], PRESSED_KEY * key);
 
 #endif //MAIN_C_MENU_H
